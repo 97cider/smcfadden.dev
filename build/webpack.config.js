5101150f4aefe0696cb1290c0d6a3816ee9fd73e
smcfadden150 <smcfadden150@gmail.com>
2020-06-30 21:07:55 -0400

    Started to add proper routing support

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -38,5 +38,10 @@ module.exports = {
     },
     plugins: [
       new VueLoaderPlugin()
-    ]
+    ],
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import App from './pages/Intro.vue';
+import routes from './routes';
 
 new Vue({
-  render: h => h(App),
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/NotFound.vue')
+    }
+  },
+  render (h) {
+    return h(App)
+  }
 }).$mount('#app');
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+});
